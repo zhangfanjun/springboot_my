2abb7220e87f244e4de21a43bbf2344d6f00ea98
zhang <827746342@qq.com>
2019-06-05 12:04:52 +0800

    字符串添加111

--- thymeleaf/src/main/java/com/qf/thymeleaf/Main.java
+++ thymeleaf/src/main/java/com/qf/thymeleaf/Main.java
@@ -2,7 +2,6 @@ package com.qf.thymeleaf;
 
 public class Main {
     public static void main(String[] args) {
-        String s1 = new String("zs");
-
+        String s1 = new String("zs11111");
     }
 }

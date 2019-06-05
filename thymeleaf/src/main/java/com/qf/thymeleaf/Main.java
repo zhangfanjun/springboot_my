285f042e5f299131d174dc837afee4052fcebac2
zhangfanjun <51359827+zhangfanjun@users.noreply.github.com>
2019-06-05 12:41:57 +0800

    server
    server change

--- thymeleaf/src/main/java/com/qf/thymeleaf/Main.java
+++ thymeleaf/src/main/java/com/qf/thymeleaf/Main.java
@@ -2,6 +2,6 @@ package com.qf.thymeleaf;
 
 public class Main {
     public static void main(String[] args) {
-        String s1 = new String("zs11111");
+        String s1 = new String("zs2222");
     }
 }
